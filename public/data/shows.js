631057cbcd2cd3568ebddece9ad10c7d148d87ca
Tyler James Leonhardt <me@tylerleonhardt.com>
2024-10-15 11:54:30 -0700

    spoons in

--- public/data/shows.js
+++ public/data/shows.js
@@ -1,6 +1,6 @@
 const shows = [
     {
-        title: "Spoon's In: A Last Supper",
+        title: "Spoons In: A Last Supper",
         description: "Several guests are brought together by a mysterious host—someone who knows each of them from their past. As the evening unfolds, a shocking revelation emerges: one of them will betray the host before the night is over. Can you guess who the betrayer is before it's too late? Very loosely based on a certain famous supper featuring betrayal.",
         date: new Date(2025, 1, 14, 18, 30),
         location: "Seattle, WA",

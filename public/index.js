2b9f4e7766cc865da148042a688af7cd0c4c34a7
Tyler Leonhardt <tyleonha@microsoft.com>
2024-10-22 16:36:52 -0700

    refactor out a Show

--- public/index.js
+++ public/index.js
@@ -23,31 +23,27 @@ const HeroBanner = () => html`
   </section>
 `;
 
+const Show = ({ show }) => html`
+  <${Card}>
+    <${ImageHeader} className="image-header" alt="testAlt" imageSrc="${show.image ?? './assets/images/hero-image.png'}" />
+    <${CardBody}>
+      <h3>${show.title}</h3>
+      ${show.jetCity ? html`<h5 class="center">In collaboration with <a target="_blank" href="https://www.jetcityimprov.org">Jet City Improv</a></h5>` : ''}
+      <p className="show-info">${show.date.toLocaleString('en-US', { weekday: 'short', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true })} - ${show.location}</p>
+      <p className="show-info">${show.description}</p>
+    </CardBody>
+    <${CardFooter}>
+      <a disabled=${!!show.tickets} href=${show.tickets}>${!show.tickets ? 'Tickets on sale soon!' : 'Tickets'}</a>
+    </CardFooter>
+  </Card>
+`;
+
 const ShowList = () => html`
-<h4>Upcoming Shows</h4>
-<div className="show">
-  ${shows.map(show => html`
-      <${Card}>
-        <${ImageHeader} className="image-header" alt="testAlt" imageSrc="${show.image ?? './assets/images/hero-image.png'}" />
-        <${CardBody}>
-          <h3>${show.title}</h3>
-          ${show.jetCity ? html`<h5 class="center">In collaboration with <a target="_blank" href="https://www.jetcityimprov.org">Jet City Improv</a></h5>` : ''}
-          <p className="show-info">${show.date.toLocaleString('en-US', { weekday: 'short', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true })} - ${show.location}</p>
-          <p className="show-info">${show.description}</p>
-        </CardBody>
-        ${html`
-        <${CardFooter}>
-          <a disabled=${!!show.tickets} href=${show.tickets}>${!show.tickets
-    ? 'Tickets on sale soon!'
-    : 'Tickets'
-  }</a>
-        </CardFooter>`}
-      </Card>`
-  )}
-  ${shows.length
-  ? html`<h5>More shows coming soon...</h5>`
-  : html`<h5>Upcoming shows comming soon...</h5>`}
-</div>
+  <h4>Upcoming Shows</h4>
+  <div className="show">
+    ${shows.map(show => html`<${Show} show=${show} />`)}
+    ${shows.length ? html`<h5>More shows coming soon...</h5>` : html`<h5>Upcoming shows coming soon...</h5>`}
+  </div>
 `;
 
 const Home = () => html`
@@ -56,7 +52,7 @@ const Home = () => html`
   <section className="main-content shows">
     <h3>Who are we?</h3>
     <p className="show-info">Hey there! We're Dollbrick, a Seattle-based improv group that's been making folks laugh since 2023.</p>
-    <p className="show-info">We love diving into long form improv, but we're not above some quick, silly games either. Our name came from a goofy improv scene—come see us live and maybe we'll spill the details. Expect lots of laughs and unexpected fun! 🧱🎭</p>
+    <p className="show-info">We love diving into long form improv, but we're not above some quick, silly games either. Our name came from a goofy improv scene—come see us live and maybe we'll spill the details. Expect lots of laughs and unexpected fun! ��</p>
     <${ShowList} />
   </section>
 </React.Fragment>
